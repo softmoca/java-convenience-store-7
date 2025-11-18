5135a9273aaa1bfa1112215bd672bccfefcf537a
softmoca <acyc0909@gmail.com>
2025-11-18 15:29:10 +0900

    feat(Product) : 상품 생성 및 재고 확인 기능 구현

---
+++ src/main/java/store/domain/Product.java
@@ -0,0 +1,27 @@
+package store.domain;
+
+public class Product {
+    private final String name;
+    private final int price;
+    private int promotionStock;
+    private int regularStock;
+
+    public Product(String name, int price, int promotionStock, int regularStock) {
+        this.name = name;
+        this.price = price;
+        this.promotionStock = promotionStock;
+        this.regularStock = regularStock;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getTotalStock() {
+        return promotionStock + regularStock;
+    }
+}
