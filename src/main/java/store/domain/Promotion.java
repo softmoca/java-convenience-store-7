de619073e6b9d9efebcacace3c1cfdea1aa3a83f
softmoca <acyc0909@gmail.com>
2025-11-18 15:19:30 +0900

    feat(Promotion) : 기간 확인 기능 구현

---
+++ src/main/java/store/domain/Promotion.java
@@ -0,0 +1,24 @@
+package store.domain;
+
+import java.time.LocalDate;
+
+public class Promotion {
+    private final String name;
+    private final int buy;
+    private final int get;
+    private final LocalDate startDate;
+    private final LocalDate endDate;
+
+    public Promotion(String name, int buy, int get,
+                     LocalDate startDate, LocalDate endDate) {
+        this.name = name;
+        this.buy = buy;
+        this.get = get;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public boolean isAvailable(LocalDate date) {
+        return !date.isBefore(startDate) && !date.isAfter(endDate);
+    }
+}
