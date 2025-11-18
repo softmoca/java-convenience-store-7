ba08bfa09538bfe71f989baea1e26e14aad243d7
softmoca <acyc0909@gmail.com>
2025-11-18 15:20:24 +0900

    test(Promotion): 프로모션 기간 외 날짜  테스트 추가

--- src/test/java/store/domain/PromotionTest.java
+++ src/test/java/store/domain/PromotionTest.java
@@ -20,4 +20,21 @@ class PromotionTest {
 
         assertThat(promotion.isAvailable(testDate)).isTrue();
     }
+
+    @Test
+    void 프로모션_기간_외_날짜는_적용되지_않는다() {
+        LocalDate startDate = LocalDate.of(2025, 11, 1);
+        LocalDate endDate = LocalDate.of(2025, 11, 30);
+
+        Promotion promotion = new Promotion(
+                "반짝할인", 1, 1, startDate, endDate
+        );
+
+        LocalDate beforeDate = LocalDate.of(2025, 10, 31);
+        LocalDate afterDate = LocalDate.of(2025, 12, 1);
+
+        assertThat(promotion.isAvailable(beforeDate)).isFalse();
+        assertThat(promotion.isAvailable(afterDate)).isFalse();
+    }
+
 }
