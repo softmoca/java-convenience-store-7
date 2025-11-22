ff3784ea741dc45410c7f5944077d8cc55ecfcad
softmoca <acyc0909@gmail.com>
2025-11-22 12:54:24 +0900

    refactor(StroeIntializer) : 메서드 분리

--- src/main/java/store/config/StoreInitializer.java
+++ src/main/java/store/config/StoreInitializer.java
@@ -6,6 +6,7 @@ import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import store.constant.ErrorMessage;
 import store.domain.aggregate.ConvenienceStore;
 import store.domain.entity.Product;
 import store.domain.vo.Promotion;
@@ -20,20 +21,32 @@ public final class StoreInitializer {
 
     public static ConvenienceStore initialize() {
         try {
-            String promotionsContent = Files.readString(Paths.get(PROMOTIONS_FILE));
-            String productsContent = Files.readString(Paths.get(PRODUCTS_FILE));
-
-            List<Promotion> promotions = FileParser.parsePromotions(promotionsContent);
-
-            Map<String, Promotion> promotionMap = promotions.stream()
-                    .collect(Collectors.toMap(Promotion::getName, p -> p));
-
-            Map<String, Product> products = FileParser.parseProducts(productsContent, promotionMap);
-
-            return new ConvenienceStore(products);
-
+            return createStore();
         } catch (IOException e) {
-            throw new IllegalStateException("상품 정보를 불러올 수 없습니다.", e);
+            throw new IllegalStateException(ErrorMessage.FILE_LOAD_ERROR.getMessage(), e);
         }
     }
+
+    private static ConvenienceStore createStore() throws IOException {
+        String promotionsContent = Files.readString(Paths.get(PROMOTIONS_FILE));
+        String productsContent = Files.readString(Paths.get(PRODUCTS_FILE));
+
+        Map<String, Promotion> promotionMap = parsePromotions(promotionsContent);
+        Map<String, Product> products = parseProducts(productsContent, promotionMap);
+
+        return new ConvenienceStore(products);
+    }
+
+
+    private static Map<String, Promotion> parsePromotions(String content) {
+        List<Promotion> promotions = FileParser.parsePromotions(content);
+        return promotions.stream()
+                .collect(Collectors.toMap(Promotion::getName, p -> p));
+    }
+
+    private static Map<String, Product> parseProducts(String content, Map<String, Promotion> promotionMap) {
+        return FileParser.parseProducts(content, promotionMap);
+    }
+
+
 }
