aa32557b05b154522692c4dfef600efe4508b14e
softmoca <acyc0909@gmail.com>
2025-11-22 20:35:07 +0900

    test(Product) : 간단한 생성자 및 getter 테스트 제거 후 재고 차감 테스트 추가ㅣ

--- src/test/java/store/domain/entitiy/ProductTest.java
+++ src/test/java/store/domain/entitiy/ProductTest.java
@@ -2,43 +2,44 @@ package store.domain.entitiy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 import store.domain.entity.Product;
-import store.domain.vo.Promotion;
 
 class ProductTest {
 
-    @Test
-    void 상품_생성_및_재고_확인() {
-        // 콜라: 프로모션 재고 10개, 일반 재고 10개
-        Product product = new Product("콜라", 1000, 10, 10);
-
-        assertThat(product.getName()).isEqualTo("콜라");
-        assertThat(product.getPrice()).isEqualTo(1000);
-        assertThat(product.getTotalStock()).isEqualTo(20);
-    }
 
     @Test
     void 재고보다_많이_구매할_수_없다() {
+        // given
         Product product = new Product("콜라", 1000, 10, 10);
 
+        // when&then
         assertThat(product.canPurchase(20)).isTrue();
         assertThat(product.canPurchase(21)).isFalse();
     }
 
     @Test
-    void 프로모션이_있는_상품_생성() {
-        Promotion promotion = new Promotion(
-                "탄산2+1", 2, 1,
-                LocalDate.of(2025, 1, 1),
-                LocalDate.of(2025, 12, 31)
-        );
+    void 프로모션_재고_우선_차감() {
+        // given
+        Product product = new Product("콜라", 1000, 10, 10);
 
-        Product product = new Product("콜라", 1000, 10, 10, promotion);
+        // when
+        product.deductStock(5);
 
-        assertThat(product.hasPromotion()).isTrue();
-        assertThat(product.getPromotion()).isEqualTo(promotion);
+        // then
+        assertThat(product.getTotalStock()).isEqualTo(15);
+    }
+
+    @Test
+    void 프로모션_재고_부족시_일반_재고에서_차감() {
+        // given
+        Product product = new Product("콜라", 1000, 3, 10);
+
+        // when
+        product.deductStock(7);
+
+        // then
+        assertThat(product.getTotalStock()).isEqualTo(6);
     }
 
 
